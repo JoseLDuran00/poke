03fa38fbbaab1f82f8f48f84feefd6077e98a27a
Jose Duran <joselduran@outlook.com>
2023-04-28 01:38:34 -0400

    styled-components [listo]

--- src/pages/pokemones/[id].js
+++ src/pages/pokemones/[id].js
@@ -1,25 +1,42 @@
-import Link from "next/link"
-import Image from "next/image"
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 const Pokemon = ({ data }) => {
-    console.log(data)
-    return(
-        <>
-            <div>
-                <h1>{data.name}</h1>
-                <h3> N° #{data.id}</h3>
-                <Image src={data.sprites.front_default} width={200} height={200}/>
-                <Link href='/'>Volver al Inicio</Link>
-            </div>
-        </>
-    )
+  const router = useRouter()
+  console.log(router)
+
+  return (
+    <div>
+      <h1>{data.name} número #{data.id}</h1>
+      <Image src={data.sprites.front_default} width={400} height={400} />
+      <Link href="/">Volver al inicio</Link>
+    </div>
+  )
 }
 
 export default Pokemon
 
-export const getServerSideProps = async({ params }) => {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
-    const data = await response.json()
+export const getStaticProps = async ({ params }) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
+  const data = await response.json()
 
-    return { props: {data} }
+  return { props: { data } }
 }
+
+export const getStaticPaths = async () => {
+  const paths = [
+    { params: { id: '1' } },
+    { params: { id: '2' } },
+  ]
+  return {
+    paths,
+    fallback: 'blocking',
+  }
+}
+//export const getServerSideProps = async ({ params }) => {
+  //const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
+  //const data = await response.json()
+
+  //return { props: { data } }
+//}
