46a2a45ffd503f0c0dcd23d0f9f2c62f71a248b9
Jose Duran <joselduran@outlook.com>
2023-04-28 02:28:09 -0400

    cambio indexjs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Link from 'next/link'
 
-const inter = Inter({ subsets: ['latin'] })
+import Link from 'next/link'
 
 const Pokemon = ({ pokemon }) =>{
   const id = pokemon.url.split('/').filter(x => x).pop()
